3ffb6f46964a2d9ac77f9ce8846fc760032ce4bd
chetta chetta chetta <>
2019-03-30 21:38:20 +0700

    refactor code

--- src/Fizzbuzz.php
+++ src/Fizzbuzz.php
@@ -2,14 +2,11 @@
     class FizzBuzz
     {
         public function say($num) {
-            if($num == 3) {
+            if($num % 3 == 0) {
                 return "Fizz";
             }
             if($num == 5) {
                 return "Buzz";
-            }
-            if($num == 6) {
-                return "Fizz";
             }
             return $num;
         }
