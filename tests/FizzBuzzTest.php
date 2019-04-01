f8632ac2d006a4e3a72edb1aea8f9ea8613505d0
AnuchitO <anuchitp@thoughtworks.com>
2019-04-01 11:48:40 +0700

    refactor test to table testing.

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -8,154 +8,44 @@ class FizzBuzzTest extends TestCase {
         $this->object = new FizzBuzz;
     }
 
-    public function testResultShouldBe1WhenEnter1()
+    public function testShouldBePassAllTheTestCases()
     {
-        $this->assertEquals(1, $this->object->say(1));
-    }
+        $testCases = array(
+            1 => 1,
+            2 => 2,
+            3 => "Fizz",
+            4 => 4,
+            5 => "Buzz",
+            6 => "Fizz",
+            7 => 7,
+            8 =>  8,
+            9 =>  "Fizz",
+            10 =>  "Buzz",
+            11 =>  11,
+            12 =>  "Fizz",
+            13 =>  13,
+            14 =>  14,
+            15 =>  "FizzBuzz",
+            16 =>  16,
+            17 =>  17,
+            18 =>  "Fizz",
+            19 =>  19,
+            20 =>  "Buzz",
+            21 =>  "Fizz",
+            22 =>  22,
+            23 =>  23,
+            24 =>  "Fizz",
+            25 =>  "Buzz",
+            26 =>  26,
+            27 =>  "Fizz",
+            28 =>  28,
+            29 =>  29,
+            30 =>  "FizzBuzz"
+        );
 
-    public function testResultShouldBe2WhenEnter2()
-    {
-        $this->assertEquals(2, $this->object->say(2));
-    }
+        foreach($testCases as $num => $expected) {
+            $this->assertEquals($expected, $this->object->say($num));
+        }
 
-    public function testResultShouldBeFizzWhenEnter3()
-    {
-        $this->assertEquals("Fizz", $this->object->say(3));
     }
-
-    public function testResultShouldBe4WhenEnter4()
-    {
-        $this->assertEquals(4, $this->object->say(4));
-    }
-
-    public function testResultShouldBeBuzzWhenEnter5()
-    {
-        $this->assertEquals("Buzz", $this->object->say(5));
-    }
-
-    public function testResultShouldBeFizzWhenEnter6()
-    {
-        $this->assertEquals("Fizz", $this->object->say(6));
-    }
-
-    public function testResultShouldBe7WhenEnter7()
-    {
-        $this->assertEquals(7, $this->object->say(7));
-    }
-
-    public function testResultShouldBe8WhenEnter8()
-    {
-        $this->assertEquals(8, $this->object->say(8));
-    }
-
-    public function testResultShouldBeFizzWhenEnter9()
-    {
-        $this->assertEquals("Fizz", $this->object->say(9));
-    }
-
-    public function testResultShouldBe10WhenEnter10()
-    {
-        $this->assertEquals("Buzz", $this->object->say(10));
-    }
-
-    public function testResultShouldBe11WhenEnter11()
-    {
-        $this->assertEquals(11, $this->object->say(11));
-    }
-
-    public function testResultShouldBeFizzWhenEnter12()
-    {
-        $this->assertEquals("Fizz", $this->object->say(12));
-    }
-
-    public function testResultShouldBe13WhenEnter13()
-    {
-        $this->assertEquals(13, $this->object->say(13));
-    }
-
-    public function testResultShouldBe14WhenEnter14()
-    {
-        $this->assertEquals(14, $this->object->say(14));
-    }
-
-    public function testResultShouldBeFizzBuzzWhenEnter15()
-    {
-        $this->assertEquals("FizzBuzz", $this->object->say(15));
-    }
-
-    public function testResultShouldBe16WhenEnter16()
-    {
-        $this->assertEquals(16, $this->object->say(16));
-    }
-
-    public function testResultShouldBe17WhenEnter17()
-    {
-        $this->assertEquals(17, $this->object->say(17));
-    }
-
-    public function testResultShouldBeFizzWhenEnter18()
-    {
-        $this->assertEquals("Fizz", $this->object->say(18));
-    }
-
-    public function testResultShouldBe19WhenEnter19()
-    {
-        $this->assertEquals(19, $this->object->say(19));
-    }
-
-    public function testResultShouldBeBuzzWhenEnter20()
-    {
-        $this->assertEquals("Buzz", $this->object->say(20));
-    }
-
-    public function testResultShouldBeFizzWhenEnter21()
-    {
-        $this->assertEquals("Fizz", $this->object->say(21));
-    }
-
-    public function testResultShouldBe22WhenEnter22()
-    {
-        $this->assertEquals(22, $this->object->say(22));
-    }
-
-    public function testResultShouldBe23WhenEnter23()
-    {
-        $this->assertEquals(23, $this->object->say(23));
-    }
-
-    public function testResultShouldBeFizzWhenEnter24()
-    {
-        $this->assertEquals("Fizz", $this->object->say(24));
-    }
-
-    public function testResultShouldBeBuzzWhenEnter25()
-    {
-        $this->assertEquals("Buzz", $this->object->say(25));
-    }
-
-    public function testResultShouldBe26WhenEnter26()
-    {
-        $this->assertEquals(26, $this->object->say(26));
-    }
-
-    public function testResultShouldBeFizzWhenEnter27()
-    {
-        $this->assertEquals("Fizz", $this->object->say(27));
-    }
-
-    public function testResultShouldBe28WhenEnter28()
-    {
-        $this->assertEquals(28, $this->object->say(28));
-    }
-
-    public function testResultShouldBe29WhenEnter29()
-    {
-        $this->assertEquals(29, $this->object->say(29));
-    }
-
-    public function testResultShouldBeFizzBuzzWhenEnter30()
-    {
-        $this->assertEquals("FizzBuzz", $this->object->say(30));
-    }
-
 }
